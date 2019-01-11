f570df88189c29689f22de6cfc6ec61e9394e8a2
fernanen <fernanen@oregonstate.edu>
2019-01-11 13:27:37 -0800

    added insertion function

--- HW1/insertsort.py
+++ HW1/insertsort.py
@@ -1,5 +1,19 @@
 import os
 import re
+
+def insertionSort(dataArray, numToAdd):
+    dataArray.append(numToAdd)
+    counter = (len(dataArray)-1)
+    if counter <= 0:
+        return dataArray
+    else:
+    while dataArray[counter] < dataArray[counter-1]:
+        tempValStore = dataArray[counter]
+        dataArray[counter] = dataArray[counter-1]
+        dataArray[counter-1] = tempValStore
+        counter = counter-1
+    return dataArray
+
 fileLocation = os.getcwd() + "/data.txt"
 print fileLocation
 myfile = open(fileLocation,"r")
@@ -8,7 +22,8 @@ for line in myfile:
     numset = line.split(' ',1)[0]
     dataset = line.split(' ',1)[1]
     print "numbers in the set: " + numset + "dataset: " + dataset  
-    for number in dataset.split()
-        print number
+    sortedList = []
+    for number in dataset.split(''):
+        insertionSort(sortedList,numToAdd)
     print "line done"
 print"done"
