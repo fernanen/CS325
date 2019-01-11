6cfc8bbaea98947a2dbb723840bc1fd920a8257f
fernanen <fernanen@oregonstate.edu>
2019-01-11 11:23:52 -0800

    parsed line.

--- HW1/insertsort.py
+++ HW1/insertsort.py
@@ -5,8 +5,10 @@ print fileLocation
 myfile = open(fileLocation,"r")
 counter = 1 
 for line in myfile:
-    line.split()
-    for number in line
+    numset = line.split(' ',1)[0]
+    dataset = line.split(' ',1)[1]
+    print "numbers in the set: " + numset + "dataset:" dataset  
+    for number in dataset.split()
         print number
     print "line done"
 print"done"
