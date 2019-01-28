734fc486656fcd8f3aca41617aadd0e21d2bcbde
fernanen <fernanen@oregonstate.edu>
2019-01-28 11:18:57 -0800

    Added print statements

--- HW3/shopping.py
+++ HW3/shopping.py
@@ -25,10 +25,12 @@ myfile = open(fileLocation,"r")
 #read number of test cases
 numcases = myfile.readline()
 numcases = int(numcases)
+print("number of cases: {}").format(numcases)
 caseList = []
 for cases in range(numcases):
     numItems = myfile.readline()
     numItems = int(numItems)
+    print("number of items: {}").format(numItems)
     itemList = [0] * numItems
 
     for item in range(numItems):
@@ -37,11 +39,15 @@ for cases in range(numcases):
         itemInfo = list(map(int, itemLine.split()))
         itemList[item][price] = itemInfo[price]
         itemList[item][weight] = itemInfo[weight]
+        print("item price: {} item weight: {}").format( itemList[item][price], itemList[item][weight])
     familySize = myfile.readline()
     familySize = int(familySize)
+    print("family Size: {}").format(familySize)
     family = [0] * familySize
     for familyMember in range(familySize):
         family[familyMember] = myfile.readline()
+        family[familyMember] = int(family[familyMember])
+        print("number of items: {}").format(family[familyMember])
     #
     ##  
     #
