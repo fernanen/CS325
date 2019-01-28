85feee97e0a4dc008fdfe0827053987654713b4c
fernanen <fernanen@oregonstate.edu>
2019-01-28 13:21:54 -0800

    finished shopping

--- HW3/shopping.py
+++ HW3/shopping.py
@@ -14,7 +14,7 @@ def shopping(itemList,capacity,itemToView,keyPairCap):
     
     else if itemList[itemToView][weight] > capacity:
         result = shopping(itemList,capacity,(itemToView-1),keyPairCap)
-        
+
     else:
         tmp1 = shopping(itemList,capacity,itemToView,keyPairCap)
         tmp2 =  shopping(itemList,(capacity - itemList[itemToView][weight]),(itemToView-1),keyPairCap)
@@ -33,30 +33,49 @@ myfile = open(fileLocation,"r")
 numcases = myfile.readline()
 numcases = int(numcases)
 print("number of cases: {}").format(numcases)
-caseList = []
+
 for cases in range(numcases):
+    # Get number of items in each case
     numItems = myfile.readline()
     numItems = int(numItems)
     print("number of items: {}").format(numItems)
-    itemList = [0] * numItems
 
-    for item in range(numItems):
+    #store item information
+    itemList = [0] * (numItems+1)
+    for item in range(1,(numItems+1)):
         itemList[item] = [0] * 2
         itemLine = myfile.readline()
         itemInfo = list(map(int, itemLine.split()))
         itemList[item][price] = itemInfo[price]
         itemList[item][weight] = itemInfo[weight]
         print("item price: {} item weight: {}").format( itemList[item][price], itemList[item][weight])
+    
+    # get family size 
     familySize = myfile.readline()
     familySize = int(familySize)
     print("family Size: {}").format(familySize)
+
+    #store family member capacity
     family = [0] * familySize
+    
+
+
     for familyMember in range(familySize):
         family[familyMember] = myfile.readline()
         family[familyMember] = int(family[familyMember])
         print("weight capacity: {}").format(family[familyMember])
-    #
-    ##  
+
+    #let each member shop -- calling shopping method
+    cap = max[family] + 1
+    itemCount = len(itemList)
+    #store cases for dynamic programming application
+    keyPairCap = [] * itemCount  
+    for result in range(keyPairCap):
+        itemList[item] = [] * cap
+
+    for shopper in range(len(family)):
+        finalShop = shopping(itemList,family[familyMember],numItems,keyPairCap)
+        print("{}: total won:{} items:{}").format(shopper,finalShop[0],finalShop[1])
     #
     #with open('results.txt',"a+") as resultFile:
     #    for item in sortedList:
