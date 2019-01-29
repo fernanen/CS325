25b4ddd0c48e2882439894f9db05a7d5874150bb
fernanen <fernanen@oregonstate.edu>
2019-01-29 01:24:53 -0800

    adding back negative sign

--- HW3/shopping.py
+++ HW3/shopping.py
@@ -4,7 +4,7 @@ import re
 def shopping(capacity,itemToView):
     result = []
     price = total = 0
-    weight = items = 1
+    weight = items = -1
     difference = 1
     global itemList
     global keyPairCap
@@ -107,7 +107,7 @@ for cases in range(numcases):
         keyPairCap[result] = [None] * cap
     #print ("{} x {}").format(len(keyPairCap),len(keyPairCap[0]))
     for shopper in range(len(family)):
-        finalShop = shopping(family[familyMember],1)
+        finalShop = shopping(family[familyMember],numItems)
         print("{}: --{}--").format(shopper,finalShop)
     #
     #with open('results.txt',"a+") as resultFile:
