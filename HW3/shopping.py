c02d17189653bf816b8e54c819d09a5718027f97
fernanen <fernanen@oregonstate.edu>
2019-01-29 01:21:58 -0800

    modulorized  variable

--- HW3/shopping.py
+++ HW3/shopping.py
@@ -5,7 +5,7 @@ def shopping(capacity,itemToView):
     result = []
     price = total = 0
     weight = items = 1
-    difference = 1
+    difference = -1
     global itemList
     global keyPairCap
     
