ce555b9a607449f19ab546492cf349666a2c09db
fernanen <fernanen@oregonstate.edu>
2019-01-31 14:57:18 -0800

    Adding case numbers and EOF conditions

--- HW4/act.py
+++ HW4/act.py
@@ -18,65 +18,60 @@ def selection_sort(arr):
 
 fileLocation = os.getcwd() + "/act.txt"
 print fileLocation
-myfile = open(fileLocation,"r")
-
-#
-# wrap this with an EOF condition
-#+
-
+#myfile = open(fileLocation,"r")
 #
 # variable declarations
 #
 taskNumber = 0
 startTime= 1
 finishTime= 2
+case = 1
+with open(fileLocation,r) as myfile:
+    #get number of tasks
+    numberOfTasks = int(myfile.readline())
+    
+    #
+    # initialize information array
+    #
+    info = [None] * (numberOfTasks)
+    print ("Number of tasks: {}").format(numberOfTasks)
+    for row in range(len(info)):
+        info[row] = [-1] * 3 # 3 being number of pieces of information given. 
 
-#get number of tasks
-numberOfTasks = int(myfile.readline())
+    #
+    #getInfo
+    #
+    for task in range(len(info)):
+        line = myfile.readline()
+        line_array = list(map(int, line.split()))
+        info[task][taskNumber] = line_array[taskNumber]
+        info[task][startTime] = line_array[startTime]
+        info[task][finishTime] = line_array[finishTime]
 
-#
-# initialize information array
-#
+    print "Before sorting info {}".format(info)
+    #
+    #sort task information based on finishtime in decending order 
+    #
+    selection_sort(info)
+    print "after sorting Info {}".format(info)
 
-info = [None] * (numberOfTasks)
-print ("Number of tasks: {}").format(numberOfTasks)
-for row in range(len(info)):
-    info[row] = [-1] * 3 # 3 being number of pieces of information given. 
+    # create schedule 
+    taskList = []
 
-for task in range(len(info)):
-    line = myfile.readline()
-    line_array = list(map(int, line.split()))
-    info[task][taskNumber] = line_array[taskNumber]
-    info[task][startTime] = line_array[startTime]
-    info[task][finishTime] = line_array[finishTime]
+    #
+    # Add first task to schedule
+    #
+    taskList.insert(0,info[0][taskNumber])
+    nextLatest = info[0][startTime]
+    counter = 1 
+    while nextLatest > 0 and counter < numberOfTasks:
+        print ("couter: {}").format(counter)
+        if info[counter][finishTime] <= nextLatest:
+            nextLatest = info[counter][startTime]
+            taskList.insert(0,info[counter][taskNumber])
+        counter = counter + 1 
 
-print "Before sorting info {}".format(info)
-#
-#sort task information based on finishtime in decending order 
-#
-selection_sort(info)
-print "after sorting Info {}".format(info)
-
-# create schedule 
-taskList = []
-
-#
-# Add first task to schedule
-#
-taskList.insert(0,info[0][taskNumber])
-nextLatest = info[0][startTime]
-counter = 1 
-while nextLatest > 0 and counter < numberOfTasks:
-    print ("couter: {}").format(counter)
-    if info[counter][finishTime] <= nextLatest:
-        nextLatest = info[counter][startTime]
-        taskList.insert(0,info[counter][taskNumber])
-    counter = counter + 1 
-
-print ("Number of activities selected = {}").format(len(taskList))
-print ("tasks to run {}").format(taskList)
-
-    #with open('merge.txt',"a+") as resultFile:
-    #    for item in sortedList:
-    #        resultFile.write("%s " % item)
-    #    resultFile.write("\n")
+    print ("case {}").format(case)
+    print ("Number of activities selected = {}").format(len(taskList))
+    print ("tasks to run {}").format(taskList)
+    case = case + 1
